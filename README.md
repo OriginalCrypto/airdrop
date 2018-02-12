6c7caefddac3f7f21e8d203686abc2fc10bb6cd6
mishfit <mishochu@gmail.com>
2018-02-12 10:00:31 -0700

    make a github pages branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# airdrop
-CryptoCoin coming at ya!
--- src/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-import jQuery from 'jquery';
-
-const Bootstrap = function () {
-  window.$ = window.jQuery = jQuery;
-  return Object.hasOwnProperty.call(window, '$');
-};
-
-export default Bootstrap();
-
